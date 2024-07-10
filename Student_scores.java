9dbbef8cff0b453401b7617ed67fa4a40e5571cb
Dukeomokhui <68600399+Dukeomokhui@users.noreply.github.com>
2024-07-11 02:23:43 +0800

    Project begins

---
+++ Student_scores.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Student_scores here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student_scores {
+    public static void main(String[] args) {
+    
+    }
+}

61b00f74364b5d54bce627ba87b4fb69283e2239
Dukeomokhui <68600399+Dukeomokhui@users.noreply.github.com>
2024-07-29 10:23:33 +0800

    troubleshooting

--- Assessment_2.java
+++ Assessment_2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 
 public class Assessment_2 {
    public static void main(String[] args) {
-        String fileName = "prog5001_students_grade_2022.csv"; // Name of the file to read
+        String fileName = "prog5001_students_grade_2022"; // Name of the file to read
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             String unitName = null;

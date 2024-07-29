b7bb4b0eaaf309d9d4bde75978825504e1c7dc15
Dukeomokhui <68600399+Dukeomokhui@users.noreply.github.com>
2024-07-29 09:37:36 +0800

    Update Assessment_2.java

--- Assessment_2.java
+++ Assessment_2.java
@@ -5,5 +5,6 @@ import java.io.BufferedReader;
 public class Assessment_2 {
    public static void main(String[] args) {
         String fileName = "prog5001_students_grade_2022.csv"; // Name of the file to read
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName)))
     }
 }

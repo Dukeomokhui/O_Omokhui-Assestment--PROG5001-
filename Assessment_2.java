61106e4d8f869b012066b675058d14278d1de761
Dukeomokhui <68600399+Dukeomokhui@users.noreply.github.com>
2024-07-29 09:34:13 +0800

    Update Assessment_2.java

--- Assessment_2.java
+++ Assessment_2.java
@@ -1,10 +1,5 @@
+import java.io.IOException;
 
-/**
- * Write a description of class Assessment_2 here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Assessment_2 {
    public static void main(String[] args) {
         String fileName = "prog5001_students_grade_2022.csv"; // Name of the file to read

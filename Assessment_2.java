a8757cc5fb81efe499373fa48aceeb54971af73d
Dukeomokhui <68600399+Dukeomokhui@users.noreply.github.com>
2024-08-01 09:06:53 +0800

    Update Assessment_2.java

--- Assessment_2.java
+++ Assessment_2.java
@@ -9,7 +9,6 @@ public class Assessment_2 {
         String studentId;
         }
    public static void main(String[] args) {
-        String fileName = "prog5001_students_grade_2022.txt"; // Name of the file to read
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             String unitName = null;
